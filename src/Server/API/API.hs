09916dc2464c7f0e778388909fc0470dd7dc347b
Steven MacCoun <theraccoun@gmail.com>
2019-07-13 13:26:51 -0400

    Sample getting blog post by id

--- src/Server/API/API.hs
+++ src/Server/API/API.hs
@@ -7,9 +7,12 @@ import Entities
 import Effects
 import Polysemy
 import Database.Persist (Entity(..), entityValues)
-import Database.Persist.Sql (rawSql)
+import Database.Persist.Sql (rawSql, toSqlKey)
+import Database.Persist.Types (PersistValue(..))
 
-type API = "blog_post" :> Get '[JSON] [BlogPost]
+type API =
+       "blog_post" :> Get '[JSON] [BlogPost]
+  :<|> "blog_post" :> Capture "blogPostId" Int64 :> Get '[JSON] (Maybe BlogPost)
 
 api :: Proxy API
 api = Proxy
@@ -19,5 +22,13 @@ getBlogPosts = do
   bps <- runSql $ rawSql @(Entity BlogPost) "SELECT ?? FROM blog_post" []
   return $ entityVal <$> bps
 
+getBlogPost :: Int64 -> '[Db] >@> Maybe BlogPost
+getBlogPost bpId = do
+  bps <- runSql $ rawSql @(Entity BlogPost) "SELECT ?? FROM blog_post WHERE id=?" [PersistInt64 bpId]
+  return $
+    nonEmpty bps
+    & fmap head
+    & fmap entityVal
+
 apiServer :: ServerT API (Sem AllAppEffects)
-apiServer = getBlogPosts
+apiServer = getBlogPosts :<|> getBlogPost
